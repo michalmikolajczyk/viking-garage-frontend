654be9f52fc902bfb417a68fae1ef0b6f59d8360
Jan Horubała <jan.horubala@gmail.com>
2017-02-02 15:34:24 +0100

    Implemented Detail view, plus fixed menu and line under it

--- src/ts/components/Container.tsx
+++ src/ts/components/Container.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {AppBar} from 'material-ui';
+import AppBarLine from 'components/AppBarLine';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import Menu from 'components/Menu';
 import Search from 'components/Search';
+
+const barStyle = {
+  position: 'fixed',
+};
 
 const titleStyle = {
   fontFamily: 'Junicode',
@@ -16,11 +21,13 @@ export default function Container(props) {
      <div>
       <AppBar
         zDepth={0} // switch off box-shadow
+        style={barStyle}
         title={<Link to="/">&#5809;</Link>}
         titleStyle={titleStyle}
         showMenuIconButton={false}
         iconElementRight={<Menu />}
       />
+      <AppBarLine />
       <Header />
       <Search />
       <div className="container">
--- src/ts/components/Detail.tsx
+++ src/ts/components/Detail.tsx
@@ -1,10 +1,76 @@
 import React from 'react';
 import Container from 'components/Container';
 
+const item = {
+  image: 'http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Honda-CRF-450R-Motocross-Bike-PNG-Image.png',
+  title: 'KTM 250SX 2017',
+  engine: '2-stroke',
+  capacity: '249',
+  maxPower: '45',
+  torque: '28',
+  seatHeight: '960',
+}
+
 export default function Detail(props) {
+
 	return (
     <Container>
-      <h1>Detail view</h1>
+      <div className="detail">
+        <div className="image">
+          <img src={item.image} />
+        </div>
+        <div className="params">
+          <div className="title">
+            {item.title}
+          </div>
+          <div className="trait">
+            <div className="name">
+              Engine Type:
+            </div>
+            <div className="value">
+              {item.engine}
+            </div>
+          </div>
+          <div className="trait">
+            <div className="name">
+              Capacity:
+            </div>
+            <div className="value">
+              {item.capacity}cc
+            </div>
+          </div>
+          <div className="trait">
+            <div className="name">
+              Max Power:
+            </div>
+            <div className="value">
+              {item.maxPower}HP
+            </div>
+          </div>
+          <div className="trait">
+            <div className="name">
+              Torque:
+            </div>
+            <div className="value">
+              {item.torque}Nm
+            </div>
+          </div>
+          <div className="trait">
+            <div className="name">
+              Seat Height:
+            </div>
+            <div className="value">
+              {item.seatHeight}mm
+            </div>
+          </div>
+          <button className="ride-btn">
+            <span className="raido">
+              &#5809;
+            </span>
+             IDE
+          </button>
+        </div>
+      </div>
     </Container>
   );
 }
