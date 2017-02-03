c4d158c3f095834ebbd6464d6cce8ec9184cedfb
Jan Horubała <jan.horubala@gmail.com>
2017-02-03 23:40:04 +0100

    Change onScroll listener to requestAnimationFrame

--- src/ts/components/Container.tsx
+++ src/ts/components/Container.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {AppBar} from 'material-ui';
-import AppBarLine from 'components/AppBarLine';
+import AppBar from 'components/AppBar';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import Menu from 'components/Menu';
 import Search from 'components/Search';
-
-const barStyle = {
-  position: 'fixed',
-};
-
-const titleStyle = {
-  fontFamily: 'Junicode',
-  color: '#AD000D'
-};
 
 export default function Container(props) {
   return (
      <div>
-      <AppBar
-        zDepth={0} // switch off box-shadow
-        style={barStyle}
-        title={<Link to="/">&#5809;</Link>}
-        titleStyle={titleStyle}
-        showMenuIconButton={false}
-        iconElementRight={<Menu />}
-      />
-      <AppBarLine />
+      <AppBar />
       <Header />
       <Search />
       <div className="container">
