1f12fdb74055ff42a33034d6efd0e7d96fba3896
Jan Horubała <jan.horubala@gmail.com>
2017-02-03 23:03:01 +0100

    Added polyfill in js for requestAnimationFrame

---
+++ src/js/pollyfills.js
@@ -0,0 +1,12 @@
+// requestAnimFrame shim  with setTimeout fallback
+// http://stackoverflow.com/a/13002988/6041704
+window.requestAnimationFrame = (function () {
+return window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+  };
+})();
