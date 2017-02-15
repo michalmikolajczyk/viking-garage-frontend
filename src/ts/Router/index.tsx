3df76f7d2b628d4404726714f7f6fc3d29d31e1c
Jan Horubała <jan.horubala@gmail.com>
2017-02-15 19:24:26 +0100

    Added Login view from previous project

--- src/ts/Router/index.tsx
+++ src/ts/Router/index.tsx
@@ -7,14 +7,15 @@ import {
 import Offers from '../Offers'
 import Detail from '../Detail'
 import NotFound from '../NotFound'
+import Login from '../Login'
 
 export default function AppRouter(props) {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={Offers} />
       <Route path="/detail" component={Detail} />
+      <Route path="/login" component={Login} />
       <Route path="/signup" component={NotFound} />
-      <Route path="/login" component={NotFound} />
       <Route path="/bike-owners" component={NotFound} />
       <Route path="/guides-coaches" component={NotFound} />
       <Route path="/mechanics" component={NotFound} />
