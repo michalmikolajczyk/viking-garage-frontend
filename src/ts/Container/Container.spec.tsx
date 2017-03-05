0861e296e2d3f28130e53147d4cf51cbfd08ce5b
Jan Horubała <jan.horubala@gmail.com>
2017-03-05 21:34:21 +0100

    Implemented tests for Container

---
+++ src/ts/Container/Container.spec.tsx
@@ -0,0 +1,24 @@
+import 'mocha';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import Container from './index';
+import AppBarVG from '../AppBar';
+import Header from '../Header';
+import Footer from '../Footer';
+import { mountWithTheme } from '../helpers/test-theme';
+
+describe('<Container />', () => {
+  it('check if all subcomponents are rendered', () => {
+    const wrapper = mountWithTheme(<Container />);
+    expect(wrapper.find(AppBarVG)).to.have.length(1);
+    expect(wrapper.find(Header)).to.have.length(1);
+    expect(wrapper.find(Footer)).to.have.length(1);
+  });
+
+  it('check if children are rendered', () => {
+    const wrapper = mountWithTheme(<Container><div className="children"></div></Container>);
+    expect(wrapper.find('.children')).to.have.length(1);
+  });
+});
