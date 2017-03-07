6f5e96fcb57b555213ea723bb8fe6f097f8661b8
Jan Horubała <jan.horubala@gmail.com>
2017-03-07 13:16:16 +0100

    Created mockup with KTM SX info

---
+++ src/ts/Detail/mockup.ts
@@ -0,0 +1,50 @@
+const ktm = {
+  title: 'KTM SX 125 2012',
+  type: 'Cross',
+  location: {
+    address: {
+      district: 'mazowieckie',
+      city: 'Warsaw',
+      Area: 'Srodmiescie',
+      Country: 'Poland',
+    },
+    cord: {
+      x: '',
+      y: '',
+    },
+    google: '',
+  },
+  rating: {
+    runes: 5,
+    votes: 120,
+  },
+  owner: {
+    name: 'Michal Mikolajczyk',
+    picture: '',
+    badges: [
+      'recommended',
+      'trusted',
+      'confirmed',
+    ],
+  },
+  images: {
+    main: 'http://www.totalmotorcycle.com/motorcycles/2012models/2012-KTM-125SXd.jpg',
+  },
+  price: {
+    unit: {
+      hour: 30,
+      day: 55,
+      week: 200,
+    },
+    value: 'USD',
+  },
+  hashtags: [
+    'motocross',
+    'ktm',
+    'warsaw',
+  ],
+};
+
+export {
+  ktm,
+};
