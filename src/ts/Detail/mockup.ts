8977095d5c0951fb542b008d66e0d6eaf9d7ab08
Jan Horubała <jan.horubala@gmail.com>
2017-03-07 17:08:15 +0100

    Added husaberg & freeride to mockup

--- src/ts/Detail/mockup.ts
+++ src/ts/Detail/mockup.ts
@@ -1,5 +1,7 @@
 const ktm = {
-  title: 'KTM SX 125 2012',
+  url: 'ktm-sx-125-poland-warsaw',
+  title: 'KTM SX 125, 2012',
+  approx: '3 km away',
   type: 'Cross',
   description: 'A brand new MX1 class two-stroke. Fantastic steering, light, and brings enough power to compete with 450cc four-stroke motocross bikes.',
   location: {
@@ -46,6 +48,108 @@ const ktm = {
   ],
 };
 
+const husaberg = {
+  url: 'husaberg-fe-390-poland-warsaw',
+  title: 'Husaberg FE 390 2010',
+  approx: '3 km away',
+  type: 'Cross',
+  description: 'Can less really be more with the radical Husaberg design? Small changes give the FE 390 a totally different personality than its big brothers.',
+  location: {
+    address: {
+      district: 'mazowieckie',
+      city: 'Warsaw',
+      area: 'Srodmiescie',
+      country: 'Poland',
+    },
+    cord: {
+      x: '',
+      y: '',
+    },
+    google: '',
+  },
+  rating: {
+    runes: 5,
+    votes: 120,
+  },
+  owner: {
+    fullname: 'Michal Mikolajczyk',
+    picture: 'https://scontent-waw1-1.cdninstagram.com/t51.2885-15/e35/15877344_344661349253204_5404343355553349632_n.jpg',
+    badges: [
+      'recommended',
+      'trusted',
+      'confirmed',
+    ],
+  },
+  images: {
+    main: 'http://moto.zombdrive.com/images/husaberg-fe-390-2.jpg',
+  },
+  price: {
+    unit: {
+      hour: 30,
+      day: 55,
+      week: 200,
+    },
+    value: 'USD',
+  },
+  hashtags: [
+    'motocross',
+    'ktm',
+    'warsaw',
+  ],
+};
+
+const freeride = {
+  url: 'freeride-250r-poland-warsaw',
+  title: 'KTM Freeride 250R 2017',
+  approx: '3 km away',
+  type: 'Cross',
+  description: 'All those who have never had the opportunity to enjoy the punch of a powerful two-stroke in a lightweight, agile offroad chassis are in for a surprise with the Freeride 250 R. However, those who have already had this pleasure know: The power of such an engine develops completely differently, the expanding combustion gases effectively pressing down twice as often on the crankshaft as a four-stroke.',
+  location: {
+    address: {
+      district: 'mazowieckie',
+      city: 'Warsaw',
+      area: 'Srodmiescie',
+      country: 'Poland',
+    },
+    cord: {
+      x: '',
+      y: '',
+    },
+    google: '',
+  },
+  rating: {
+    runes: 5,
+    votes: 120,
+  },
+  owner: {
+    fullname: 'Jan Horubala',
+    picture: 'https://scontent-amt2-1.cdninstagram.com/t51.2885-15/e35/16463985_256259144802357_4986204357729976320_n.jpg',
+    badges: [
+      'recommended',
+      'trusted',
+      'confirmed',
+    ],
+  },
+  images: {
+    main: 'https://foto.scigacz.pl/cache/imgs/_w720/gallery/mod_static/KTM-Freeride-250R_18956_1.jpg',
+  },
+  price: {
+    unit: {
+      hour: 30,
+      day: 55,
+      week: 200,
+    },
+    value: 'USD',
+  },
+  hashtags: [
+    'motocross',
+    'ktm',
+    'warsaw',
+  ],
+};
+
 export {
   ktm,
+  husaberg,
+  freeride,
 };
