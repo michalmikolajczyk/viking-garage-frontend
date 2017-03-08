275920da87555dee529a5b32571b4dd75e5065de
Jan Horubała <jan.horubala@gmail.com>
2017-03-08 11:52:28 +0100

    Removed whatshot button, all card is a link

--- src/ts/Offers/Offer.tsx
+++ src/ts/Offers/Offer.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as _ from 'lodash';
 import { Link } from 'react-router';
-import { FontIcon } from 'material-ui';
 
 export default function Offer(props) {
   const {
@@ -12,22 +11,16 @@ export default function Offer(props) {
   const img = _.get(props, 'data.images.main');
   const price = `${props.data.price.unit.day} $ / day`;
 
-  const actionIconButton = url => (
-    <div className="offer-btn">
-      <Link to={`offer/${url}`} className="offer-btn-link">
-        <FontIcon className="material-icons">whatshot</FontIcon>
-      </Link>
-    </div>);
-
   return (
-    <div className="card">
-      <div style={{ backgroundImage: `url(${img})` }} className="image"/>
-      <div className="head">
-        <div className="descript">
-          <div className="title">{title}</div>
-          <div className="foot">{`${price} ${approx}`}</div>
+    <Link to={`offer/${url}`} className="offer-btn-link">
+      <div className="card">
+        <div style={{ backgroundImage: `url(${img})` }} className="image"/>
+        <div className="head">
+          <div className="descript">
+            <div className="title">{title}</div>
+            <div className="foot">{`${price} ${approx}`}</div>
+          </div>
         </div>
-        {actionIconButton(url)}
       </div>
-    </div>);
+    </Link>);
 }
