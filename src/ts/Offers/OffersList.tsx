a858df8ba4dcf59897c0f98c06ccd81f3e545c4b
Jan Horubała <jan.horubala@gmail.com>
2017-03-08 12:03:40 +0100

    Removed 'load more' button

--- src/ts/Offers/OffersList.tsx
+++ src/ts/Offers/OffersList.tsx
@@ -4,13 +4,11 @@ import { CircularProgress } from 'material-ui';
 import Offer from './Offer';
 
 export default function OffersList(props) {
-
   const {
     data,
     loadMore,
     loading,
   } = props;
-
   const loader = loading ? (
       <CircularProgress size={60} thickness={7} color="white" />
     ) : (
@@ -18,16 +16,13 @@ export default function OffersList(props) {
         Load more results
       </button>
     );
-
+  const loadItem = (<div className="load">{loader}</div>);
   const mappedItems = data.map(item => <Offer data={item} key={item.url} />);
 
   return (
     <div className="offers-wrap">
       <div className="offers cont">
         {mappedItems}
-      </div>
-      <div className="load">
-        {loader}
       </div>
     </div>
   );
