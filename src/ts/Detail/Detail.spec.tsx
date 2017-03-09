64dc623d9d4dc3cf51eebb1a96f39d4c6ced1675
Jan Horubała <jan.horubala@gmail.com>
2017-03-09 17:52:59 +0100

    Updated test for Detail

--- src/ts/Detail/Detail.spec.tsx
+++ src/ts/Detail/Detail.spec.tsx
@@ -2,13 +2,13 @@ import 'mocha';
 import * as React from 'react';
 import { expect } from 'chai';
 import { default as Detail } from './index';
-import { mountWithTheme } from '../helpers/test-theme';
+import { shallow } from 'enzyme';
 
 describe('<Detail />', () => {
-  it('check for inner components: Container and traits', () => {
-    const wrapper = mountWithTheme(<Detail params={{ offer: 'husaberg-fe-390-poland-warsaw' }} />);
+  it('check for inner components: Container, Offer and Form', () => {
+    const wrapper = shallow(<Detail params={{ offer: 'husaberg-fe-390-poland-warsaw' }} />);
     expect(wrapper.find('Container')).to.have.length(1);
-    expect(wrapper.find('.image')).to.have.length(1);
-    expect(wrapper.find('.ride-btn')).to.have.length(1);
+    expect(wrapper.find('DetailOffer')).to.have.length(1);
+    expect(wrapper.find('DetailForm')).to.have.length(1);
   });
 });
