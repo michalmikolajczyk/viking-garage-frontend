a03886cfde46b2d0e35d3334203ccceba5685536
Jan Horubała <jan.horubala@gmail.com>
2017-03-09 17:26:02 +0100

    Added test for Detail/Comments

---
+++ src/ts/Detail/Comments.spec.tsx
@@ -0,0 +1,18 @@
+import 'mocha';
+import * as React from 'react';
+import { expect } from 'chai';
+import { default as Comments } from './Comments';
+import { shallow } from 'enzyme';
+
+describe('<Comments />', () => {
+  it('check for inner components: comments, rate, author', () => {
+    const wrapper = shallow(<Comments />);
+    expect(wrapper.find('.comments')).to.have.length(1);
+    expect(wrapper.find('.type')).to.have.length(4);
+    expect(wrapper.find('.view-all')).to.have.length(1);
+    const author = wrapper.find('.author').first();
+    expect(author.find('.picture')).to.have.length(1);
+    expect(author.find('.fullname')).to.have.length(1);
+    expect(author.find('.location')).to.have.length(1);
+  });
+});
