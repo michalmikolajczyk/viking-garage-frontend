3b8dbbabf45ee8ef2b1c9bd86fe7d582569f2b48
Jan Horubała <jan.horubala@gmail.com>
2017-03-15 14:47:37 +0100

    Added more Accordions, pass accordion info as props

--- src/ts/Detail/Accordion.tsx
+++ src/ts/Detail/Accordion.tsx
@@ -9,25 +9,6 @@ import {
   TableRowColumn,
 } from 'material-ui';
 
-const items = [
-  ['Cylinder count', '1'],
-  ['Bore / Stroke', '55 mm'],
-  ['Transmission', '6 gears, MT'],
-  ['Compression Ratio', 'xxx'],
-  ['Suspension', 'Sanchez'],
-  ['Frame', 'Alloy'],
-  ['Wheelbase', '1597 mm'],
-  ['Rake, Trail', 'wtf'],
-  ['Wheel sizes', '21" F, 19" R'],
-  ['Tires', 'EXCEL'],
-  ['Brakes', 'Brembo'],
-  ['Fuel Capacity', '7.5 l'],
-  ['Ignition Type', 'Kickstarr'],
-  ['Kickstand', 'No'],
-  ['Lights', 'No'],
-  ['Storage Space', 'No'],
-];
-
 function createTable(items) {
   const half = Math.ceil(items.length/2);
   const table = _.map(_.range(half), (index) => (
@@ -42,35 +23,47 @@ function createTable(items) {
     <Table selectable={false}>
       <TableBody displayRowCheckbox={false}>
         {table}
+        <TableRow />
       </TableBody>
     </Table>);
 }
 
 export default class Accordion extends React.Component<any, any> {
+  private header: any;
   private tablebody: any;
 
   constructor(props) {
     super(props);
-    this.state = { open: true };
+    const {
+      open,
+      items,
+      header,
+    } = props;
+    this.state = { open };
+    this.header = header;
     this.tablebody = createTable(items);
+    this.toggle = this.toggle.bind(this);
   }
+
+  toggle() { this.setState({ open: !this.state.open }); }
 
   render() {
     const icon = this.state.open
       ? <FontIcon className="material-icons">expand_less</FontIcon>
       : <FontIcon className="material-icons">keyboard_arrow_down</FontIcon>
-    const tablebody = this.state.open ? this.tablebody : null
+    const header = this.header && (
+      <div className="header">
+        <FlatButton
+          icon={icon}
+          disableTouchRipple={true}
+          onTouchTap={this.toggle}
+        >{this.header}</FlatButton>
+      </div>);
 
     return (
       <div className="accordion">
-        <div className="header">
-          <FlatButton
-            icon={icon}
-            disableTouchRipple={true}
-            onTouchTap={() => this.setState({ open: !this.state.open })}
-          >Motorcycle additional specs</FlatButton>
-        </div>
-        {tablebody}
+        {header}
+        {this.state.open ? this.tablebody : null}
       </div>
     );
   }
--- src/ts/Detail/DetailList.tsx
+++ src/ts/Detail/DetailList.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
 import Accordion from './Accordion';
 
+const items = [
+  ['Cylinder count', '1'],
+  ['Bore / Stroke', '55 mm'],
+  ['Transmission', '6 gears, MT'],
+  ['Compression Ratio', 'xxx'],
+  ['Suspension', 'Sanchez'],
+  ['Frame', 'Alloy'],
+  ['Wheelbase', '1597 mm'],
+  ['Rake, Trail', 'wtf'],
+  ['Wheel sizes', '21" F, 19" R'],
+  ['Tires', 'EXCEL'],
+  ['Brakes', 'Brembo'],
+  ['Fuel Capacity', '7.5 l'],
+  ['Ignition Type', 'Kickstarr'],
+  ['Kickstand', 'No'],
+  ['Lights', 'No'],
+  ['Storage Space', 'No'],
+];
+
 export default function DetailList(props) {
   const { offer } = props;
   return (
-    <Accordion offer={offer} />
+    <div>
+      <Accordion items={items} open={true} header="Motorcycle additional specs" />
+      <Accordion items={items} open={false} header="Mechanical state" />
+      <Accordion items={items} open={false} header="Available protection" />
+      <Accordion items={items} open={false} header="Accessories" />
+      <Accordion items={items} open={false} header="Optional services" />
+    </div>
   );
 }
