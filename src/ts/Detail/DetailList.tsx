9bad9e94a6d76b633d10805ee6d980f2baeff80c
Jan Horubała <jan.horubala@gmail.com>
2017-03-15 19:34:24 +0100

    Remove material ui Table, create own Accordion Tables

--- src/ts/Detail/DetailList.tsx
+++ src/ts/Detail/DetailList.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as _ from 'lodash';
-import Accordion from './Accordion';
+import Accordion from '../Accordion';
 
 export default function DetailList({ offer }) {
   const { specs } = offer;
   const accordions = _.map(specs, (item, index) => (
       <Accordion
         key={index}
-        open={index === 0}
+        open={false}
         header={item.title}
         items={item.value}
       />
