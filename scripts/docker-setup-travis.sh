e0973d6f836779d0674746f7062dc5a393548923
Michał Mikołajczyk <michal@trewebs.com>
2017-03-19 01:19:56 +0100

    testing docker Travis CI

--- scripts/docker-setup-travis.sh
+++ scripts/docker-setup-travis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t viking-garage-frontend-image ./
-docker run -e TRAVIS=1 -p 127.0.0.1:80:3000 --name viking-garage-frontend-container viking-garage-frontend-image
+docker build -t vikinggarage/viking-garage-frontend:latest ./
+docker run -p 127.0.0.1:80:3000 --name viking-garage-frontend -e TRAVIS="1" vikinggarage/viking-garage-frontend:latest
 # docker run --name viking-garage-frontend-container viking-garage-frontend-image
