6d611be826ef67dcc85baab5ccb8e14d2b001570
Jan Horubała <jan.horubala@gmail.com>
2017-03-27 14:00:25 +0200

    Create: offer title/brief & permissions

--- src/ts/Offer/Create/index.tsx
+++ src/ts/Offer/Create/index.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import Container from '../../Container';
 import Select from './Select';
 import DropImage from './DropImage';
+import {
+  FlatButton,
+  Paper,
+  TextField,
+} from 'material-ui';
 
 export default class Create extends React.Component<any, any> {
   constructor(props) {
@@ -19,7 +24,33 @@ export default class Create extends React.Component<any, any> {
       <Container close={true}>
         <div className="create cont">
           <Select selected={this.state.selected} onChange={this.onSelect} />
+          <Paper className="section">
+            <TextField
+              floatingLabelText="Offer Title"
+              fullWidth={true}
+            />
+            <TextField
+              floatingLabelText="Description (max. 555 characters)"
+              multiLine={true}
+              rows={3}
+              fullWidth={true}
+              rowsMax={3}
+            />
+          </Paper>
           <DropImage />
+          <Paper className="section">
+            <div className="header">Permissions</div>
+            <div className="body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.
+            </div>
+            <div className="buttons">
+              <FlatButton label="More info" />
+              <div>
+                <FlatButton label="Decline" />
+                <FlatButton label="Accept" />
+              </div>
+            </div>
+          </Paper>
         </div>
       </Container>
     );
