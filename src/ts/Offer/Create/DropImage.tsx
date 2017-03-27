21844000acd0213219be84b0adab3b1d81b670db
Jan Horubała <jan.horubala@gmail.com>
2017-03-27 13:36:37 +0200

    Create: Select Offer & Drop Image

---
+++ src/ts/Offer/Create/DropImage.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import {
+  FontIcon,
+  Paper,
+} from 'material-ui'
+import * as Dropzone from 'react-dropzone';
+
+export default class DropImage extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(images, event) {
+    this.setState({ images });
+  }
+
+  render() {
+    const images = this.state.images.map((file, i) => (
+      <div
+        key={i}
+        className="image"
+        style={{ backgroundImage: `url(${file.preview})` }}
+      />));
+
+    return (
+      <Paper className="section image-upload">
+        <div className="header">Upload image</div>
+        <Dropzone
+          className="drop-area"
+          activeClassName="over"
+          accept="image/*"
+          onDrop={this.onDrop}>
+          <FontIcon className="fa fa-picture-o" />
+          Drop files to upload
+        </Dropzone>
+        <div className="gallery">
+          {images}
+        </div>
+      </Paper>);
+  }
+}
--- src/ts/Offer/Create/index.tsx
+++ src/ts/Offer/Create/index.tsx
@@ -1,53 +1,25 @@
 import * as React from 'react';
-import {
-  DropDownMenu,
-  FontIcon,
-  MenuItem,
-} from 'material-ui';
 import Container from '../../Container';
-
-const select = [
-  'accessories',
-  'helmet',
-  'motorcycle',
-  'protection',
-  'services',
-];
+import Select from './Select';
+import DropImage from './DropImage';
 
 export default class Create extends React.Component<any, any> {
   constructor(props) {
     super(props);
-    this.state = {
-      selected: 2,
-    };
+    this.state = { selected: 2 };
+    this.onSelect = this.onSelect.bind(this);
   }
 
-
-  handleChange() {}
+  onSelect(event, index, selected) {
+    this.setState({ selected });
+  }
 
   render() {
-    const selectList = select.map((s, i) => <MenuItem key={i} value={i} primaryText={s} />)
-
     return (
       <Container close={true}>
         <div className="create cont">
-          <div className="title">
-            <span className="text">Add new offer:</span>
-            <DropDownMenu
-              value={this.state.selected}
-              onChange={this.handleChange}
-              labelStyle={{ fontSize: 34, textDecoration: 'underline', paddingLeft: 14 }}
-              listStyle={{ marginLeft: 10 }}
-              underlineStyle={{ borderTop: 'none' }}
-              iconStyle={{ display: 'none' }}
-            >
-              {selectList}
-            </DropDownMenu>
-            <FontIcon
-              className="material-icons"
-              style={{ fontSize: 34 }}
-            >keyboard_arrow_down</FontIcon>
-        </div>
+          <Select selected={this.state.selected} onChange={this.onSelect} />
+          <DropImage />
         </div>
       </Container>
     );
