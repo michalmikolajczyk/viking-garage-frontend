793e9bcc8c649b87a4f4ab20872f872891afaad4
Jan Horubała <jan.horubala@gmail.com>
2017-03-29 14:48:31 +0200

    Menu translation

--- src/ts/i18n/pl.ts
+++ src/ts/i18n/pl.ts
@@ -1,8 +1,21 @@
 export default {
 // Header
   'rent motorcycles and gear': 'wynajem motocykli i sprzętu',
-  'find mechanics, coaches, trails': 'mechanicy, trenerzy, trasy',
+  'find mechanics, coaches, trails': 'mechanicy, trenerzy i trasy',
   'everywhere in the world': 'gdziekolwiek na świecie',
 
+// Menu
+  'Profile': 'Profil',
+  'Add offer': 'Dodaj ofertę',
+  'Deals': 'Transakcje',
+  'Messages': 'Wiadomości',
+  'Account': 'Konto',
+  'Log out': 'Wyloguj',
+  'Sign in': 'Stwórz konto',
+  'Log in': 'Zaloguj się',
+  'Bike Owners': 'Motocykliści',
+  'Guides & Coaches': 'Przewodnicy i Trenerzy',
+  'Mechanics': 'Mechanicy',
+  'FAQ': 'FAQ',
 
 }
