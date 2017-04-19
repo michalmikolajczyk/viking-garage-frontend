cb7e6de25c010bf871841f2365517c71f2ebdbca
Jan Horubała <jan.horubala@gmail.com>
2017-04-19 23:22:21 +0200

    Implemented logic of multiselect

--- src/ts/Search/Select.tsx
+++ src/ts/Search/Select.tsx
@@ -12,45 +12,83 @@ import i from '../i18n';
 
 export default class Select extends React.Component<any, any> {
   state = {
-    group: [],
-    value: [],
-  }
+    values: [],
+  };
 
   selectionRenderer = (values) => {
-    switch (values.length) {
-      case 0:
-        return '';
-      default:
-        return `${values.length} names selected`;
-    }
+    return [...values].sort().reduce((acc, curr, index, arr) => {
+      if (curr.indexOf('#') === -1) {
+        return [curr, [...acc[1], `${curr.toUpperCase()}, `]]
+      } else {
+        return [acc[0], [...acc[1], `${curr.split('#')[1]}, `]]
+      }
+    }, [undefined, []])[1]
   }
 
-  menuItems = () => _.keys(items).map((key, index) => (
-    [
-      <MenuItem
-        key={key}
-        value={key}
-      >
-        <Toggle
-          className="menu-toggle"
-          label={key}
-        />
-      </MenuItem>,
-      ...items[key].map(subkey => (
+  menuItems = () => items.map((item) => {
+    if (item.indexOf('#') > -1) {
+      return (
         <MenuItem
-          key={`${key}#${subkey}`}
+          key={item}
           insetChildren={true}
-          checked={false}
-          value={subkey}
-          primaryText={subkey}
-        />
-      )),
-    ]
-  ))
+          checked={this.state.values.indexOf(item) > -1}
+          value={item}
+          primaryText={item.split('#')[1]}
+        />)
+    } else {
+      return (
+        <MenuItem
+          key={item}
+          value={item}
+        >
+          <Toggle
+            label={item}
+            className="menu-toggle"
+            toggled={this.state.values.indexOf(item) > -1}
+          />
+        </MenuItem>
+      )
+    }
+  })
 
   onChange = (event, index, values) => {
-    this.setState({ values });
-    this.props.filter(values)
+    // Toggle inside MenuItem invoke onChange twice - once with event object
+    if (!_.isArray(values)) return;
+
+    let diff = _.difference(this.state.values, values)[0];
+    if (diff) {
+      if (diff.indexOf('#') === -1) {
+        return this.setState({
+          values: values.filter(v => v.indexOf(diff) === -1)
+        });
+      } else {
+        let group = diff.split('#')[0];
+        return this.setState({
+          values: values.filter(v => v !== group)
+        });
+      }
+    }
+    diff = _.difference(values, this.state.values)[0];
+    if (diff) {
+      if (diff.indexOf('#') === -1) {
+        this.setState({
+          values: [
+            ...values,
+            ...items.filter(i => i.indexOf(`${diff}#`) > -1),
+          ]
+        })
+      } else {
+        let group = diff.split('#')[0];
+        let subset = items.filter(i => i.indexOf(`${group}#`) > -1)
+        return this.setState({
+          values: [
+            ...values,
+            ...(_.difference(subset, values).length === 0 ? [group] : []),
+          ]
+        })
+      }
+    }
+    return this.setState({ values });
   }
 
   render() {
@@ -61,20 +99,19 @@ export default class Select extends React.Component<any, any> {
           <SelectField
             multiple={true}
             hintText="Select type"
-            value={[]}
+            value={this.state.values}
             onChange={this.onChange}
             selectionRenderer={this.selectionRenderer}
           >
             {this.menuItems()}
           </SelectField>
         </div>
-        <hr />
       </div>
     );
   }
 }
 
-const items = {
+const rawItems = {
   'Motorcycle': [
     'Off-road',
     'Street',
@@ -116,3 +153,8 @@ const items = {
     'Clubs',
   ],
 };
+
+const items = _.flatten(_.keys(rawItems).map((item) => [
+  item,
+  ...rawItems[item].map(i => `${item}#${i}`),
+]))
