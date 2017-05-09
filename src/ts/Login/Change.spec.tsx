9efdd667088cf4e97d3e55c02f06a4f3b9a534e8
Jan Horubała <jan.horubala@gmail.com>
2017-05-09 14:40:38 +0200

    Tests for Login/Change

---
+++ src/ts/Login/Change.spec.tsx
@@ -0,0 +1,95 @@
+import 'mocha';
+import * as React from 'react';
+import * as chai from 'chai';
+import { shallow } from 'enzyme';
+import * as sinon from 'sinon';
+require('sinon-stub-promise')(sinon);
+import * as sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const expect = chai.expect;
+import { formsyContext } from '../helpers/test-theme';
+import * as api from './api';
+import Change from './Change';
+
+const paramToken = 'uuid-token';
+const userPassword = 'secret';
+
+describe('Login: <Change />', () => {
+  it('check for inner components', () => {
+    const wrapper = shallow(<Change params={{ token: paramToken }} />, formsyContext());
+    expect(wrapper.find('.form')).to.have.length(1);
+    expect(wrapper.find('FormsyText')).to.have.length(2);
+    expect(wrapper.find('[name="password1"]')).to.have.length(1);
+    expect(wrapper.find('[name="password2"]')).to.have.length(1);
+    expect(wrapper.find('button.submit')).to.have.length(1);
+  });
+
+  it('check if submit action get user info properly', () => {
+    const wrapper = shallow(<Change params={{ token: paramToken }} />, formsyContext());
+    const instance = wrapper.instance();
+    const stub = sinon.stub(api, 'change', ({ token, password1, password2 }) => {
+      expect(token).to.be.equal(paramToken);
+      expect(password1).to.be.equal(userPassword);
+      expect(password2).to.be.equal(userPassword);
+      return Promise.resolve('ok');
+    });
+    instance['submit']({
+      password1: userPassword,
+      password2: userPassword,
+    });
+    stub.restore();
+  });
+
+  it('check if submit action works properly', () => {
+    const wrapper = shallow(<Change params={{ token: paramToken }} />, formsyContext());
+    const instance = wrapper.instance();
+
+    const stub = sinon.stub(api, 'change')['returnsPromise']();
+    instance.setState = sinon.spy(instance.setState);
+    expect(instance.setState).to.not.have.been.called;
+
+    instance['submit']();
+    stub.resolves('ok');
+
+    expect(instance.setState).to.have.been.calledOnce;
+    expect(instance.state.changeSuccess).to.be.true;
+    expect(instance.state.changeError).to.be.false;
+    expect(instance.state.networkErr).to.be.false;
+    sinon.restore(api.change);
+  });
+
+  it('check if submit action show backend error', () => {
+    const wrapper = shallow(<Change params={{ token: paramToken }} />, formsyContext());
+    const instance = wrapper.instance();
+
+    const stub = sinon.stub(api, 'change')['returnsPromise']();
+    instance.setState = sinon.spy(instance.setState);
+    expect(instance.setState).to.not.have.been.called;
+
+    instance['submit']();
+    stub.resolves({ err: 'no internet connection'});
+
+    expect(instance.setState).to.have.been.calledOnce;
+    expect(instance.state.changeSuccess).to.be.false;
+    expect(instance.state.changeError).to.be.true;
+    expect(instance.state.networkErr).to.be.false;
+    sinon.restore(api.change);
+  });
+
+  it('check if submit action show unexpected network error', () => {
+    const wrapper = shallow(<Change params={{ token: paramToken }} />, formsyContext());
+    const instance = wrapper.instance();
+
+    const stub = sinon.stub(api, 'change')['returnsPromise']();
+    instance.setState = sinon.spy(instance.setState);
+    expect(instance.setState).to.not.have.been.called;
+
+    instance['submit']();
+    stub.rejects('something gone wrong');
+
+    expect(instance.setState).to.have.been.calledOnce;
+    expect(instance.state.changeSuccess).to.be.false;
+    expect(instance.state.changeError).to.be.false;
+    expect(instance.state.networkErr).to.be.true;
+  });
+});
