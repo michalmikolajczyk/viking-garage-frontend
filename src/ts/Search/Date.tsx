baf0069e59f17face8df75c7b8ed54be1a522037
Jan Horubała <jan.horubala@gmail.com>
2017-05-12 19:24:34 +0200

    Update code style in Search/Date

--- src/ts/Search/Date.tsx
+++ src/ts/Search/Date.tsx
@@ -14,23 +14,21 @@ export default class DateVG extends React.Component<any, any> {
       // get a two weeks forward
       endDate: new Date(now.getTime() + 14 * 24 * 60 * 60 * 1000),
     };
-    this.onChangeStart = this.onChangeStart.bind(this);
-    this.onChangeEnd = this.onChangeEnd.bind(this);
   }
 
-  public updateFilter() {
+  updateFilter = () => {
     this.props.filter({
       startDate: this.state.startDate,
       endDate: this.state.endDate,
     });
   }
 
-  public onChangeStart(event, date) {
+  onChangeStart = (event, date) => {
     this.setState({ startDate: date });
     this.updateFilter();
   }
 
-  public onChangeEnd(event, date) {
+  onChangeEnd = (event, date) => {
     this.setState({ endDate: date });
     this.updateFilter();
   }
@@ -41,7 +39,6 @@ export default class DateVG extends React.Component<any, any> {
         <FontIcon className="material-icons">today</FontIcon>
         <DatePicker
           className="filter"
-          // automatically accept and close the picker on select
           autoOk={true}
           value={this.state.startDate}
           onChange={this.onChangeStart}
