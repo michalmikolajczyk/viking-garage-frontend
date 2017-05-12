dc4dc6261fc30dbddc90afd7bec37c7b6b3875c5
Jan Horubała <jan.horubala@gmail.com>
2017-05-12 19:35:54 +0200

    Tests for Search/Date

---
+++ src/ts/Search/Date.spec.tsx
@@ -0,0 +1,55 @@
+import 'mocha';
+import * as React from 'react';
+import * as chai from 'chai';
+import { shallow } from 'enzyme';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+const expect = chai.expect;
+import * as moment from 'moment';
+import DateVG from './Date';
+
+describe('Search <Date />', () => {
+  it('check for inner components', () => {
+    const wrapper = shallow(<DateVG />);
+    expect(wrapper.find('.date')).to.have.length(1);
+    expect(wrapper.find('.filter')).to.have.length(2);
+    expect(wrapper.find('DatePicker')).to.have.length(2);
+  });
+
+  it('check if sets actual date on create (to the minute accuracy)', () => {
+    const start = moment().toDate().toString()
+    const end = moment().add(14, 'days').toDate().toString();
+    const wrapper = shallow(<DateVG />);
+    expect(wrapper.state().startDate.toString()).to.be.equal(start);
+    expect(wrapper.state().endDate.toString()).to.be.equal(end);
+  });
+
+  it('check if updates filter on change start date', () => {
+    const start = moment().toDate();
+    const filter = sinon.spy((params) => {
+      const {
+        startDate,
+        endDate,
+      } = params;
+      expect(startDate).to.be.equal(start);
+    });
+    const wrapper = shallow(<DateVG filter={filter} />);
+    const instance = wrapper.instance();
+    instance['onChangeStart'](undefined, start);
+  });
+
+  it('check if update filter on change end date', () => {
+    const end = moment().toDate();
+    const filter = sinon.spy((params) => {
+      const {
+        startDate,
+        endDate,
+      } = params;
+      expect(endDate).to.be.equal(end);
+    });
+    const wrapper = shallow(<DateVG filter={filter} />);
+    const instance = wrapper.instance();
+    instance['onChangeEnd'](undefined, end);
+  });
+});
