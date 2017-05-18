4d9d0906ff10ee8e04e2d2f5f2532534f059a581
Michal Mikolajczyk <michal@trewebs.com>
2017-05-18 19:11:51 +0200

    api url kludge

--- src/ts/API/index.tsx
+++ src/ts/API/index.tsx
@@ -1,4 +1,4 @@
-const url = process.env.API_URL || 'http://localhost:4000';
+const url = location.hostname.match('localhost') ? 'http://localhost:4000' : 'https://viking-garage-api-dev.herokuapp.com';
 
 const API = {
 
