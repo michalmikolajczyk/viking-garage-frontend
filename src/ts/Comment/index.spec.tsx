f5d010914eae9ffdcefe9b1baa51650a8cdd965b
Jan Horubała <jonzee@users.noreply.github.com>
2017-05-25 00:23:24 +0200

    Removed timeout from test (issue #29) (#48)
    * Removed timeout from test (issue #29)
    
    * remove console log

--- src/ts/Comment/index.spec.tsx
+++ src/ts/Comment/index.spec.tsx
@@ -1,24 +1,13 @@
 import 'mocha';
 import * as React from 'react';
 import { expect } from 'chai';
+import { shallow } from 'enzyme';
 import { default as Comments } from './';
-import { mountWithTheme } from '../helpers/test-theme';
 
 describe('<Comments />', () => {
-  it('check for inner components: comments, rate, author', function (done) {
-    this.timeout(3000);
-    const wrapper = mountWithTheme(<Comments />);
+  it('check for inner component', () => {
+    const wrapper = shallow(<Comments />);
     expect(wrapper.find('.comments')).to.have.length(1);
-    setTimeout(
-      () => {
-        expect(wrapper.find('.view-all')).to.have.length(1);
-        expect(wrapper.find('.type')).to.have.length(4);
-        const author = wrapper.find('.author').first();
-        expect(author.find('.picture')).to.have.length(1);
-        expect(author.find('.fullname')).to.have.length(1);
-        expect(author.find('.location')).to.have.length(1);
-        done();
-      },
-      2500);
+    expect(wrapper.find('Paper')).to.have.length(1);
   });
 });
