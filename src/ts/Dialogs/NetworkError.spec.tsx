0e5fe0f7f5bacd54c840bce7fd5a51d3e19185cd
Jan Horubała <jan.horubala@gmail.com>
2017-05-28 10:18:56 +0200

    Fixes in test

--- src/ts/Dialogs/NetworkError.spec.tsx
+++ src/ts/Dialogs/NetworkError.spec.tsx
@@ -11,8 +11,8 @@ chai.use(sinonChai);
 
 describe('<NetworkError />', () => {
   it('check for inner components: text and action buttons', () => {
-    const title = 'Network Error';
-    const body = 'Ups! It seems that we have some problems with network, try again later.';
+    const title = 'We’re not connected';
+    const body = 'The Garage is not connected to the network. Connect your device to the network or try again later.';
     const wrapper = mountWithTheme(<NetworkError open={true} close={() => undefined} />);
     expect(wrapper.find('Dialog').prop('title')).to.equal(title);
     expect(wrapper.find('Dialog').prop('children')).to.equal(body);
