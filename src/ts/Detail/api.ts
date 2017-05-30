3d08842cc79d30283f29db414ef108c1477fce9f
Jan Horubała <jan.horubala@gmail.com>
2017-05-30 11:46:27 +0200

    Restructured FormVG & Ride booking flow

--- src/ts/Detail/api.ts
+++ src/ts/Detail/api.ts
@@ -6,12 +6,3 @@ export function get(id) {
   })
   .then(res => res.json());
 }
-
-export function ride(data) {
-  return window['fetch'](`${API.ride}/`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-  })
-  .then(res => res.json());
-}
