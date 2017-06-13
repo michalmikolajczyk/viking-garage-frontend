3b99efa5b5f789b4c7df6a6091ff8846813690b4
Jan Horubała <jan.horubala@gmail.com>
2017-06-13 23:50:49 +0200

    Menu RWD (hid user profile func)

--- src/ts/Menu/index.tsx
+++ src/ts/Menu/index.tsx
@@ -6,6 +6,7 @@ import {
 import {
   Divider,
   FontIcon,
+  IconButton,
   IconMenu,
   MenuItem,
   Paper,
@@ -13,55 +14,64 @@ import {
 import i from '../i18n';
 
 export default function MenuVG(props) {
-  function logout() {
-    localStorage.removeItem('jwt');
-    localStorage.removeItem('user');
-    props.update();
-    browserHistory.push('/');
-  };
+  // function logout() {
+  //   localStorage.removeItem('jwt');
+  //   localStorage.removeItem('user');
+  //   props.update();
+  //   browserHistory.push('/');
+  // };
 
-  const user = typeof localStorage !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : '';
-  const image = user && user['image'];
+  // const user = typeof localStorage !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : '';
+  // const image = user && user['image'];
 
-  const iconButtonElement = image
-  ? <MenuItem className="user-image" style={{ background: `url(${image})` }}/>
-  : <FontIcon className="material-icons">account_circle</FontIcon>
+  // const iconButtonElement = image
+  // ? <MenuItem className="user-image" style={{ background: `url(${image})` }}/>
+  // : <FontIcon className="material-icons">account_circle</FontIcon>
 
-  const paperStyle = {
-    height: 30,
-    width: 30,
-  }
+  // const paperStyle = {
+  //   height: 30,
+  //   width: 30,
+  // }
 
-  const userSection = user ? (
-    <IconMenu
-      className="user-profile"
-      iconButtonElement={<Paper style={paperStyle} circle={true}>{iconButtonElement}</Paper>}
-      anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-      desktop={true}
-    >
-      <MenuItem primaryText={i('Profile')} containerElement={<Link to="/user/profile" />} />
-      <MenuItem primaryText={i('Add offer')} containerElement={<Link to="/add-offer" />} />
-      <MenuItem primaryText={i('Deals')} containerElement={<Link to="/deals" />} />
-      <MenuItem primaryText={i('Messages')} containerElement={<Link to="/user/inbox" />} />
-      <MenuItem primaryText={i('Account')} containerElement={<Link to="/user/account" />} />
-      <Divider />
-      <MenuItem primaryText={i('Log out')} onClick={logout} />
-    </IconMenu>
-  ) : (
-    <div className="user">
-      <MenuItem className="menu-item" containerElement={<Link to="/signin" />} primaryText={i('Sign in')} />
-      <MenuItem className="menu-item" containerElement={<Link to="/login" />} primaryText={i('Log in')} />
-    </div>
-  );
+  // const userSection = user ? (
+  //   <IconMenu
+  //     className="user-profile"
+  //     iconButtonElement={<Paper style={paperStyle} circle={true}>{iconButtonElement}</Paper>}
+  //     anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+  //     desktop={true}
+  //   >
+  //     <MenuItem primaryText={i('Profile')} containerElement={<Link to="/user/profile" />} />
+  //     <MenuItem primaryText={i('Add offer')} containerElement={<Link to="/add-offer" />} />
+  //     <MenuItem primaryText={i('Deals')} containerElement={<Link to="/deals" />} />
+  //     <MenuItem primaryText={i('Messages')} containerElement={<Link to="/user/inbox" />} />
+  //     <MenuItem primaryText={i('Account')} containerElement={<Link to="/user/account" />} />
+  //     <Divider />
+  //     <MenuItem primaryText={i('Log out')} onClick={logout} />
+  //   </IconMenu>
+  // ) : (
+  //   <div className="user">
+  //     <MenuItem className="menu-item" containerElement={<Link to="/signin" />} primaryText={i('Sign in')} />
+  //     <MenuItem className="menu-item" containerElement={<Link to="/login" />} primaryText={i('Log in')} />
+  //   </div>
+  // );
 
-
-  return (
+  const menuItems = (
     <div className="menu">
       <MenuItem className="menu-item" containerElement={<Link to="/page/owner" />} primaryText={i('Bike Owners')} />
       <MenuItem className="menu-item" containerElement={<Link to="/page/guide" />} primaryText={i('Guides & Coaches')} />
       <MenuItem className="menu-item" containerElement={<Link to="/page/mechanic" />} primaryText={i('Mechanics')} />
       <MenuItem className="menu-item" containerElement={<Link to="/page/faq" />} primaryText={i('FAQ')} />
-      {userSection}
+    </div>
+  );
+
+  return (
+    <div>
+      <IconMenu className="mobile-only" iconButtonElement={<IconButton><FontIcon className="fa fa-bars" /></IconButton>}>
+        {menuItems}
+      </IconMenu>
+      <div className="mobile-hid">
+        {menuItems}
+      </div>
     </div>
   );
 }
