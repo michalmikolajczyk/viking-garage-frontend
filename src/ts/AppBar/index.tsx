152183a2d58681bb169f3a05597eb276300614c4
Jan Horubała <jan.horubala@gmail.com>
2017-06-13 23:39:25 +0200

    Created RWD view for AppBar (with styles)

--- src/ts/AppBar/index.tsx
+++ src/ts/AppBar/index.tsx
@@ -1,53 +1,22 @@
 import * as React from 'react';
 import { Link } from 'react-router';
 import { AppBar } from 'material-ui';
+import AppBarWrap from './AppBarWrap';
+import Search from '../Search';
 import Raido from '../Raido';
 import Menu from '../Menu';
-import debug from 'debug';
-const log = debug('app:AppBar');
 
-// Scroll logic based on https://gist.github.com/Warry/4254579
-export default class AppBarVG extends React.Component<any, any> {
-  private requestId: number;
-
-  constructor(props) {
-    super(props);
-    this.state = { visible: false };
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleScroll();
-  }
-
-  componentWillUnmount() {
-    window.cancelAnimationFrame(this.requestId);
-  }
-
-  handleScroll() {
-    if (this.state.visible && window.scrollY === 0) {
-      this.setState({ visible: false });
-    } else {
-      if (!this.state.visible && window.scrollY > 0) {
-        this.setState({ visible: true });
-      }
-    }
-    // there is a polyfill in js/polyfills.js
-    this.requestId = window.requestAnimationFrame(this.handleScroll);
-  }
-
-  render() {
-    const line = this.state.visible && <div className="app-bar-line"></div>;
-
-    return (
+export default function AppBarVG(props) {
+  return (
+    <AppBarWrap>
       <AppBar
-        className="app-bar"
         zDepth={0}
+        className="app-bar"
         showMenuIconButton={true}
-        iconElementLeft={<Link className={'title' + (this.state.visible ? ' border' : '')} to="/"><Raido /></Link>}
-        iconElementRight={<Menu update={() => this.forceUpdate()} />}
-      >
-        {line}
-      </AppBar>);
-  }
+        title={<Search {...props} />}
+        titleStyle={{ padding: '0 10px' }}
+        iconElementRight={<Menu />}
+        iconElementLeft={<Link to="/" className="title"><Raido /></Link>}
+      />
+    </AppBarWrap>);
 }
