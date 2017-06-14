bc6856e54adf192565114c20f24f6483ed037d78
Jan Horubała <jan.horubala@gmail.com>
2017-06-14 13:01:13 +0200

    Refactor Footer RWD

--- src/ts/Footer/index.tsx
+++ src/ts/Footer/index.tsx
@@ -11,51 +11,47 @@ const iconStyle = {
 export default function Footer(props) {
   return (
     <div className="footer">
-      <ul className="contact">
-        <li>
+      <div className="contact">
+        <div>
           <a href="https://www.instagram.com/vikinggarage" target="_blank">
             INSTAGRAM
             <span className="mobile-hid"> vikinggarage</span>
           </a>
-        </li>
-        <li>
+        </div>
+        <div>
           <a href="https://www.facebook.com/vikinggarage888" target="_blank">
             FACEBOOK
             <span className="mobile-hid"> vikinggarage888</span>
           </a>
-        </li>
-        <li>
+        </div>
+        <div>
           <a href="https://github.com/michalmikolajczyk/" target="_blank">
             GITHUB
           </a>
-        </li>
-        <li>
+        </div>
+        <div>
           <a href="mailto:ride@vikinggarage.com" target="_blank">
             E-MAIL
             <span className="mobile-hid"> ride@vikinggarage.com</span>
           </a>
-        </li>
-      </ul>
-      <ul className="contact right">
-        <li>
+        </div>
+        <div className="right">
           <a href="https://vikinggarage.slack.com" target="_blank">
             SLACK
           </a>
-        </li>
-        <li>
+        </div>
+        <div className="right">
           <a href="tel:+48697951264" target="_blank">
             PHONE
             <span className="mobile-hid"> +48 697 951 264</span>
           </a>
-        </li>
-        <li>
+        </div>
+        <div className="right">
           <a href="skype:michaljanmikolajczyk?chat" target="_blank">
             SKYPE
           </a>
-        </li>
-      </ul>
-      <div className="madeby">
-        <div className="nowrap">
+        </div>
+        <div className="copyright">
           <FontIcon style={iconStyle} color="white" className="fa fa-copyright" />
           2017 VIKING GARAGE
         </div>
