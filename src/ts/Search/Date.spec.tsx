090785a673f20ed3d824c06d71cbfab75bbd1837
Jan Horubała <jonzee@users.noreply.github.com>
2017-06-20 21:37:40 +0200

    Date validation (#74)
    * Added date validation in FormPure
    
    * Dates validation in main filter (issue #69)
    
    * Added tests for Search/Date

--- src/ts/Search/Date.spec.tsx
+++ src/ts/Search/Date.spec.tsx
@@ -45,4 +45,55 @@ describe('Search <Date />', () => {
     const instance = wrapper.instance();
     instance['onChangeEnd'](undefined, endDate);
   });
+
+  it('check if date picker before today is disabled', () => {
+    const wrongDate = moment().add(-5, 'days').toDate();
+    const okDate = moment().add(5, 'days').toDate();
+    const wrapper = shallow(<DateVG filter={() => {}} />);
+    const instance = wrapper.instance();
+    expect(instance['shouldDisableDateStart'](wrongDate)).to.be.true;
+    expect(instance['shouldDisableDateEnd'](wrongDate)).to.be.true;
+    expect(instance['shouldDisableDateStart'](okDate)).to.be.false;
+    expect(instance['shouldDisableDateEnd'](okDate)).to.be.false;
+  });
+
+  it('check if date picker is disabled after setting end date', () => {
+    const wrongStartDate = moment().add(10, 'days').toDate();
+    const okStartDate = moment().add(1, 'days').toDate();
+    const endDate = moment().add(5, 'days').toDate();
+    const wrapper = shallow(<DateVG filter={() => {}} />);
+    const instance = wrapper.instance();
+    instance['onChangeEnd'](undefined, endDate);
+    expect(instance['shouldDisableDateStart'](wrongStartDate)).to.be.true;
+    expect(instance['shouldDisableDateStart'](okStartDate)).to.be.false;
+  });
+
+  it('check if date picker is disabled after setting end date', () => {
+    const startDate = moment().add(10, 'days').toDate();
+    const wrongEndDate = moment().add(5, 'days').toDate();
+    const okEndDate = moment().add(15, 'days').toDate();
+    const wrapper = shallow(<DateVG filter={() => {}} />);
+    const instance = wrapper.instance();
+    instance['onChangeStart'](undefined, startDate);
+    expect(instance['shouldDisableDateEnd'](wrongEndDate)).to.be.true;
+    expect(instance['shouldDisableDateEnd'](okEndDate)).to.be.false;
+  });
+
+  it('check if date picker is ok between start and end date', () => {
+    const startDate = moment().add(5, 'days').toDate();
+    const endDate = moment().add(10, 'days').toDate();
+    const okMiddle =  moment().add(7, 'days').toDate();
+    const before = moment().add(3, 'days').toDate();
+    const after = moment().add(13, 'days').toDate();
+    const wrapper = shallow(<DateVG filter={() => {}} />);
+    const instance = wrapper.instance();
+    instance['onChangeStart'](undefined, startDate);
+    instance['onChangeEnd'](undefined, endDate);
+    expect(instance['shouldDisableDateEnd'](okMiddle)).to.be.false;
+    expect(instance['shouldDisableDateStart'](okMiddle)).to.be.false;
+    expect(instance['shouldDisableDateEnd'](before)).to.be.true;
+    expect(instance['shouldDisableDateStart'](before)).to.be.false;
+    expect(instance['shouldDisableDateEnd'](after)).to.be.false;
+    expect(instance['shouldDisableDateStart'](after)).to.be.true;
+  });
 });
