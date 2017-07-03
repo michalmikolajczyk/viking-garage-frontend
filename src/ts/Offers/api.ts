df54ec91a926d9b33dd93945f4beca58724fceb8
Jan Horubała <jan.horubala@gmail.com>
2017-07-03 15:50:41 +0200

    Added offset to request parameters

--- src/ts/Offers/api.ts
+++ src/ts/Offers/api.ts
@@ -4,9 +4,10 @@ import * as moment from 'moment'
 export function get(state) {
   const location = state.location ? `lat=${state.location.lat}&lng=${state.location.lng}` : '';
   const distance = state.distance ? `&dist=${state.distance * 1000}` : '';
+  const offset = `&offset=${state.offset}`;
   const type = state.type ? `&type=${state.type}` : '';
   const date = state.date ? `&start=${moment(state.date.start).unix()}&end=${moment(state.date.end).unix()}` : '';
-  const req = `${API.offer}?${location}${distance}${type}${date}`;
+  const req = `${API.offer}?${location}${distance}${offset}${type}${date}`;
 
   return fetch(req, {
     method: 'GET',
