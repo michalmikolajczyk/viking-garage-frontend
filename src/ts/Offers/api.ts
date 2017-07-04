df79ab6c1fd262a654231e15e45fb8f013f9c06b
Jan Horubała <jan.horubala@gmail.com>
2017-07-04 10:52:47 +0200

    Added paging & empty message in offers list

--- src/ts/Offers/api.ts
+++ src/ts/Offers/api.ts
@@ -5,7 +5,7 @@ export function get(state) {
   const location = state.location ? `lat=${state.location.lat}&lng=${state.location.lng}` : '';
   const distance = state.distance ? `&dist=${state.distance * 1000}` : '';
   const offset = `&offset=${state.offset}`;
-  const type = state.type ? `&type=${state.type}` : '';
+  const type = state.type ? `&type=${state.type.toLowerCase()}` : '';
   const date = state.date ? `&start=${moment(state.date.start).unix()}&end=${moment(state.date.end).unix()}` : '';
   const req = `${API.offer}?${location}${distance}${offset}${type}${date}`;
 
