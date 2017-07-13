50fad1d51be04563d5339f8f3a8fd3914cb18cec
Jan Horubała <jan.horubala@gmail.com>
2017-07-13 12:15:08 +0200

    List all unset env vars

--- server/index.js
+++ server/index.js
@@ -8,8 +8,12 @@ const appJSON = fs.readFileSync('app.json', 'utf8');
 const appConf = JSON.parse(appJSON);
 
 Object.keys(appConf.env).forEach((key) => {
+  let unset = false;
   if (process.env[key] === undefined) {
     console.error(key, 'is undefined!');
+    unset = true;
+  }
+  if (unset) {
     console.error('You should set all env vars!');
     console.error('For details check app.json and README');
     process.exit(1);
@@ -19,9 +23,9 @@ Object.keys(appConf.env).forEach((key) => {
 const serverDev = require('./server-dev');
 const serverProd = require('./server-prod');
 const port = process.env.PORT;
-const node_env = process.env.NODE_ENV;
+const nodeEnv = process.env.NODE_ENV;
 
-if (node_env === 'production') {
+if (nodeEnv === 'production') {
   serverProd.listen(port);
 } else {
   serverDev.listen(port);
