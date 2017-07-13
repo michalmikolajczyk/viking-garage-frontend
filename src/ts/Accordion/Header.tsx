9418adef6eecb57c10e05240e8f48749c2a10c60
Jan Horubała <jan.horubala@gmail.com>
2017-07-13 19:21:28 +0200

    Fixed accordios header on safari

--- src/ts/Accordion/Header.tsx
+++ src/ts/Accordion/Header.tsx
@@ -12,10 +12,11 @@ export default function Header(props) {
     <div className="acc-header">
       <FlatButton
         icon={icon || <div className="material-icons" />}
+        label={head}
+        fullWidth={true}
         onTouchTap={call}
         disableTouchRipple={true}
-      >
-        {head}
-      </FlatButton>
+        style={{ textAlign: 'left' }}
+      />
     </div>);
 }
