ac8a54658fef5805f7810706835689d372f3d707
Jan Horubała <jonzee@users.noreply.github.com>
2017-07-13 21:51:26 +0200

    Conf app (#126)
    * Added description, logo, repo etc to app.json
    
    * Added .env to gitignore
    
    * Added description to app.json env
    
    * Added dotenv & unified run script
    
    * Check all env vars before run server
    
    * Updated README with new config flow
    
    * Fixed website url & status code 1 on error
    
    * List all unset env vars
    
    * Check if env var is required
    
    * Fixed - env vars as string

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
+// read env vars from local .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+// check for required env vars declared in app.json
+const fs = require('fs');
+const appJSON = fs.readFileSync('app.json', 'utf8');
+const appConf = JSON.parse(appJSON);
+
+Object.keys(appConf.env).forEach((key) => {
+  let unset = false;
+  if (appConf.env[key].required && process.env[key] === undefined) {
+    console.error(key, 'is undefined!');
+    unset = true;
+  }
+  if (unset) {
+    console.error('You should set all env vars!');
+    console.error('For details check app.json and README');
+    process.exit(1);
+  }
+});
+
 const serverDev = require('./server-dev');
 const serverProd = require('./server-prod');
-const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV;
+const port = process.env.PORT;
+const nodeEnv = process.env.NODE_ENV;
 
-if (env === 'production') {
+if (nodeEnv === 'production') {
   serverProd.listen(port);
 } else {
   serverDev.listen(port);
