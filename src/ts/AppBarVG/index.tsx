212f9b6fb8f55d829e67ab809749b21658ae13a9
Jan Horubała <jan.horubala@gmail.com>
2017-07-21 10:45:45 +0200

    Updated AppBarVG (with search component for desktop)

--- src/ts/AppBarVG/index.tsx
+++ src/ts/AppBarVG/index.tsx
@@ -2,21 +2,29 @@ import * as React from 'react';
 import { Link } from 'react-router';
 import { AppBar } from 'material-ui';
 import AppBarWrap from './AppBarWrap';
-import Search from '../Search';
+import FullText from '../Search/FullText';
 import Raido from '../Raido';
 import Menu from '../Menu';
 
-export default function AppBarVG(props) {
+interface AppBarVGProps {
+  typeFilter: (type: any) => void;
+  dateFilter: (date: any) => void;
+  locationFilter: (location: any) => void;
+  distanceFilter: (distance: any) => void;
+}
+
+export default function AppBarVG(props: AppBarVGProps) {
   return (
     <AppBarWrap>
       <AppBar
         zDepth={0}
-        className="app-bar"
+        className="appbar"
         showMenuIconButton={true}
-        title={<Search {...props} />}
+        title={<FullText {...props} />}
         titleStyle={{ padding: '0 10px' }}
         iconElementRight={<Menu refresh={props.refresh} />}
         iconElementLeft={<Link to="/" className="title"><Raido /></Link>}
       />
-    </AppBarWrap>);
+    </AppBarWrap>
+  );
 }
