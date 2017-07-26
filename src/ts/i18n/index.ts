4ced64bf02d578ace5001a8b3ea67b3885c6d6b0
Jan Horubała <jan.horubala@gmail.com>
2017-07-26 15:18:17 +0200

    Added languages & changeLanguage function

--- src/ts/i18n/index.ts
+++ src/ts/i18n/index.ts
@@ -2,13 +2,23 @@ import pl from './pl';
 import * as moment from 'moment';
 import * as fx from 'money';
 
-const langCode = typeof navigator !== 'undefined' ? (navigator.language || navigator['userLanguage']).substr(0, 2) : 'en';
+export const languages = [
+  { code: 'pl', lang: 'Polish' },
+  { code: 'en', lang: 'English' },
+];
+
+let langCode = typeof navigator !== 'undefined' ? (navigator.language || navigator['userLanguage']).substr(0, 2) : 'en';
 moment.locale(langCode);
 const TRANSLATIONS = {
   pl,
 };
 
-export default function translate(s?: string) {
+export function changeLanguage(code) {
+  langCode = code;
+  moment.locale(code);
+}
+
+export default function i(s?: string) {
   if (typeof s === 'undefined') return langCode;
   const trans = TRANSLATIONS[langCode];
   if (trans && s in trans) return trans[s];
@@ -16,6 +26,6 @@ export default function translate(s?: string) {
 };
 
 fx.rates = {
-  'PLN': 1,
-  'USD': 0.27,
+  PLN: 1,
+  USD: 0.27,
 };
