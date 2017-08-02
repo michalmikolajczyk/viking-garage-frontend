73660cb708cd6593a02b24802b8c2e81318581e7
Jan Horubała <jan.horubala@gmail.com>
2017-08-02 15:33:25 +0200

    Moved options object out of fetch in api request

--- src/ts/Container/api.ts
+++ src/ts/Container/api.ts
@@ -3,6 +3,11 @@ import * as _ from 'lodash';
 import * as moment from 'moment';
 import { IFiltersValue } from '../typings';
 
+const options = {
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json' },
+};
+
 export function get(values: IFiltersValue, offset: number) {
   const start = _.has(values, 'date.start') ? `&start=${moment(values.date.start).unix()}` : '';
   const end = _.has(values, 'date.end') ? `&end=${moment(values.date.end).unix()}` : '';
@@ -11,9 +16,5 @@ export function get(values: IFiltersValue, offset: number) {
   const location = _.has(values, 'location.lat') ? `lat=${values.location.lat}&lng=${values.location.lng}` : '';
   const req = `${API.offer}?${location}${distance}&offset=${offset}${type}${start}${end}`;
 
-  return fetch(req, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json());
+  return fetch(req, options).then(res => res.json());
 }
