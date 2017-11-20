a8acc93456fa40fd2db3db6b6af457574e2fec3e
Michal Mikolajczyk <michal@vikinggarage.com>
2017-11-20 15:26:10 +0000

    #208 allow to store credentials cookie

--- src/ts/Login/api.tsx
+++ src/ts/Login/api.tsx
@@ -4,6 +4,7 @@ export function login(data) {
   return fetch(API.login, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    credentials: 'include', // Don't forget to specify this if you need cookies
     body: JSON.stringify(data),
   })
   .then(res => res.json());
@@ -13,6 +14,7 @@ export function reset(data) {
   return fetch(API.reset, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    credentials: 'include', // Don't forget to specify this if you need cookies
     body: JSON.stringify(data),
   })
   .then(res => res.json());
@@ -22,15 +24,15 @@ export function change(data) {
   return fetch(API.change, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    credentials: 'include', // Don't forget to specify this if you need cookies
     body: JSON.stringify(data),
   })
   .then(res => res.json());
 }
 
 export function check(token) {
-  // return fetch(API.check, {
-  //   headers: { Authorization: `JWT ${token}` },
-  // })
-  return fetch(API.check)
+  return fetch(API.check, {
+    credentials: 'include', // Don't forget to specify this if you need cookies
+  })
   .then(res => res.json());
 }
