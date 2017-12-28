c44d0ecb6be8a1b5b44f9a052d96c87ef6005834
Michał Mikołajczyk <michal.mikolajczyk@toptal.com>
2017-12-28 14:01:34 +0100

    #222 initialize GA first (#224)

--- src/ts/Main/index.tsx
+++ src/ts/Main/index.tsx
@@ -5,8 +5,13 @@ import {
 } from 'react-router';
 import routes from '../routes';
 import ReactGA from 'react-ga';
+let flag = false;
 
 export default function Main() {
+  if (process.env.GA_TRACKER && !flag) {
+    ReactGA.initialize(process.env.GA_TRACKER);
+    flag = true;
+  }
   const handleOnUpdate = () => {
     if (process.env.GA_TRACKER) {
       ReactGA.pageview(window.location.pathname + window.location.search);
