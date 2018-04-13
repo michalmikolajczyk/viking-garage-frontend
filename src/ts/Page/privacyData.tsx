12549f65780488b21a143c8a9bb97f1e0deabb0c
Michał Mikołajczyk <michal.mikolajczyk@toptal.com>
2018-04-13 09:35:49 +0200

    #252 updated privacy policy (#253)

---
+++ src/ts/Page/privacyData.tsx
@@ -0,0 +1,142 @@
+const data = {
+  title: 'FAQ',
+  data: [
+    {
+      sectionTitle: 'Introduction',
+      sectionData: [
+        {
+          title: '',
+          contents: [
+            'VIKING GARAGE respects your privacy and are committed to protecting it through our compliance with this policy.',
+            'This policy describes the types of information we may collect from you or that you may provide when you visit the website vikinggarage.com (our “Website”) and our practices for collecting, using, maintaining, protecting and disclosing that information.',
+            'Please read this policy carefully to understand our policies and practices regarding your information and how we will treat it. If you do not agree with our policies and practices, your choice is not to use our Website. By accessing or using this Website, you agree to this privacy policy. This policy may change from time to time. Your continued use of this Website after we make changes is deemed to be acceptance of those changes, so please check the policy periodically for updates.',
+          ],
+        },
+      ],
+    },
+    {
+      sectionTitle: 'Contents',
+      sectionData: [
+        {
+          title: '1.',
+          contents: [
+            'The Operator, in order to preserve the privacy of the Clients, uses appropriate technical and organisational measures to ensure the right protection of the processed data. The Operator is obliged to secure the personal data against disclosure to unauthorized persons or any other processing in the way incompatible with the law and protect against alteration, loss, damage or destruction.',
+          ],
+        },
+        {
+          title: '2.',
+          contents: [
+            'The Operator is a dataset custodian.',
+          ],
+        },
+        {
+          title: '3.',
+          contents: [
+            'Personal data filing system shall be declared to Inspector General for Personal Data Protection (or its international counterparts).',
+          ],
+        },
+        {
+          title: '4.',
+          contents: [
+            'Provision of the personal data by the Client is voluntary. Provision of information in accordance with §3 point 4 of the Terms is necessary for the conclusion of the agreement between the Operator and Client.',
+          ],
+        },
+        {
+          title: '5.',
+          contents: [
+            'Processing personal data of the Client is possible after receipt of the Client’s consent before conclusion of the agreement as well as statutory authorization for processing personal data necessary for the Service.',
+          ],
+        },
+        {
+          title: '6.',
+          contents: [
+            'The Client shall have the right to access and modify Client’s personal data.',
+          ],
+        },
+        {
+          title: '7.',
+          contents: [
+            'Personal data processing in vikinggarage.com are used only for the Operator’s Service.',
+          ],
+        },
+        {
+          title: '8.',
+          contents: [
+            'Any other information which are not Client’s personal data may be used for the promotion of vikinggarage.com. This information shall in particular consist of:',
+            [
+              'Client’s image published by Clients on the pictures or videos shared on vikinggarage.com;',
+              'Content of the offer;',
+              'Rates and reviews of Clients.',
+            ],
+          ],
+        },
+        {
+          title: '9.',
+          contents: [
+            'The Operator may use information which is not Client’s personal data in unlimited manner, in particular, to operate, disseminate and publish of advertising and promotion of vikinggarage.com, such as publication in advertisement, press articles, advertising brochures and websites.',
+          ],
+        },
+        {
+          title: '10.',
+          contents: [
+            'Clients do not have the right to remuneration for the using and publishing of information which is not Client’s personal data.',
+          ],
+        },
+        {
+          title: '11.',
+          contents: [
+            'Users may not publish any contents damaging the rights of third parties in vikinggarage.com.',
+          ],
+        },
+        {
+          title: '12.',
+          contents: [
+            'Personal data of Clients may be shared to the other Clients only to the extent necessary to carry out the Motorcycle Rental Service or Operator Service.',
+          ],
+        },
+        {
+          title: '13.',
+          contents: [
+            'Client has the right to conduct review the processing of his personal data.',
+          ],
+        },
+        {
+          title: '14.',
+          contents: [
+            'Operator provides access to up-to-date information about:',
+            [
+              'technical and organisational measures to protect personal data against accidental or unlawful destruction or unauthorised disclosure, and',
+              'the entity whom the processing of personal data has been entrusted, included scope and date of entrusting, in case of agreement between Operator and the entity whom the processing of personal data.',
+            ],
+          ],
+        },
+        {
+          title: '15.',
+          contents: [
+            'The Operator may use IP address of Clients obtained during internet connections for technical purposes connected with administering the servers. In addition IP address may use to collect general statistical information, such as information about region etc.',
+          ],
+        },
+        {
+          title: '16.',
+          contents: [
+            'The Operator may use targeting advertising of the other companies, especially such as Facebook, Google to precisely target and schedule the display of advertising. The objective of this system is to deliver adverts relevant to the Client on the basis of anonymized IP address.',
+          ],
+        },
+        {
+          title: '17.',
+          contents: [
+            'Operator uses cookies in order to collect information for statistical purposes as well as to enable the Operator to track and target the location and to deliver adverts relevant to the Client. Disabling cookies on web browser generally does not preclude use vikinggarage.com but may cause some difficulties.',
+          ],
+        },
+        {
+          title: '18.',
+          contents: [
+            'To ask questions or comment about this Privacy Policy, contact us at: info@vikinggarage.com',
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export default data;
